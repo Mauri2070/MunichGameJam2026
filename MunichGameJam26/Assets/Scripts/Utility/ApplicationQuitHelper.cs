9de82a9228b2a7795e9d4c9ba15af8a686115d7a
Georg Henning <georgm.henning@gmx.de>
2026-04-25 15:35:11 +0200

    Added ApplicationQuitHelper script.

---
+++ MunichGameJam26/Assets/Scripts/Utility/ApplicationQuitHelper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace MGJ.Utility
+{
+    public class ApplicationQuitHelper : MonoBehaviour
+    {
+        public void QuitApplication()
+        {
+            Application.Quit();
+        }
+    }
+}
